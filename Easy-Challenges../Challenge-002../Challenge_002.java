3c5de15f6f91ebcf0360105d4cdb2eca90b253d5
ieabbas <ieabbas@users.noreply.github.com>
2016-03-19 23:34:02 -0700

    Challenge 2 solution creation
    - 1st attempt at the solution

---
+++ Easy-Challenges../Challenge-002../Challenge_002.java
@@ -0,0 +1,31 @@
+package challenge_002;
+
+import java.util.*;
+
+/**
+ * This repository contains the challenges posted from the
+ * DailyProgrammerChallenges GitHub repository that can be found at
+ * https://github.com/FreddieV4/DailyProgrammerChallenges.
+ * 
+ * Challenge 002 : Hello, coders! An important part of programming is being able
+ * to apply your programs, so your challenge for today is to create a calculator
+ * application that has use in your life. It might be an interest calculator, or
+ * it might be something that you can use in the classroom. For example, if you
+ * were in physics class, you might want to make a F = M * A calc. Extra credit
+ * : allow the calculator to have multiple functions!
+ * 
+ * Solution provided by : Ismail A.
+ */
+public class Challenge_002 {
+
+	public static void main(String[] args) {
+		Calculator calc = new Calculator();
+		Scanner kb = new Scanner(System.in);
+		int input;
+		
+		//While the input doesn't equal the word stop, keep running the calculator
+		//So is input supposed to be an int or 
+		while()
+	}
+
+}
