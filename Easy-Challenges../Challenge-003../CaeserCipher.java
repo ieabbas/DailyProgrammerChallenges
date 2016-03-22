6957b296386b14f7ebdae8c85c6ae25fa4f395d7
Ismail A <ieabbas@users.noreply.github.com>
2016-03-22 14:00:47 -0700

    Create CaeserCipher.java

---
+++ Easy-Challenges../Challenge-003../CaeserCipher.java
@@ -0,0 +1,21 @@
+package challenge_003;
+
+/**
+ * This repository contains the challenges posted from the
+ * DailyProgrammerChallenges GitHub repository that can be found at
+ * https://github.com/FreddieV4/DailyProgrammerChallenges.
+ *
+ * This class will act as the object performing the encryption and decryption of
+ * any alphabetical text passed in.
+ *
+ * Solution provided by : Ismail A.
+ */
+public class CaeserCipher {
+
+	/*
+	 * This is the default constructor the the {@link CaeserCipher} class
+	 */
+	public CaeserCipher() {
+		
+	}
+}
