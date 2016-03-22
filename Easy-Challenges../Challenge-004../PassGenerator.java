97f743cb81fc596b4db83a9ee88c4bc151a89a05
Ismail A <ieabbas@users.noreply.github.com>
2016-03-22 15:51:27 -0700

    Create PassGenerator.java

---
+++ Easy-Challenges../Challenge-004../PassGenerator.java
@@ -0,0 +1,15 @@
+package challenge_004;
+
+/**
+ * This repository contains the challenges posted from the
+ * DailyProgrammerChallenges GitHub repository that can be found at
+ * https://github.com/FreddieV4/DailyProgrammerChallenges.
+ * 
+ * This class will act as the object creating the random passwords, being called
+ * upon in the Challenge_004 class.
+ * 
+ * Solution provided by : Ismail A.
+ */
+public class PassGenerator {
+
+}
