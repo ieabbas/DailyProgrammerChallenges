f356864ff2bad12e3c39b6257fd513a1fc0e3839
Ismail A <ieabbas@users.noreply.github.com>
2016-03-23 16:38:23 -0700

    Create Challenge_005.java

---
+++ Easy-Challenges../Challenge-005../Challenge_005.java
@@ -0,0 +1,24 @@
+package challenge_005;
+
+/**
+ * This repository contains the challenges posted from the
+ * DailyProgrammerChallenges GitHub repository that can be found at
+ * https://github.com/FreddieV4/DailyProgrammerChallenges.
+ * 
+ * Challenge 005 : Your challenge for today is to create a program which is
+ * password protected, and wont open unless the correct user and password is
+ * given.For extra credit, have the user and password in a seperate .txt file.
+ * For even more extra credit, break into your own program :)
+ * 
+ * Solution provided by : Ismail A.
+ * 
+ * @author ieabbas
+ */
+public class Challenge_005 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
