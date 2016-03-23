9ca53056ddcec95be6a2707eb039d3ff7180e18f
Ismail A <ieabbas@users.noreply.github.com>
2016-03-23 16:12:17 -0700

    Update PassGenerator.java
    - All that remains is to finish generate()

--- Easy-Challenges../Challenge-004../PassGenerator.java
+++ Easy-Challenges../Challenge-004../PassGenerator.java
@@ -12,4 +12,42 @@ package challenge_004;
  */
 public class PassGenerator {
 
+	/*
+	 * This variable will hold how many passwords the user wishes to generate
+	 * from the object
+	 */
+	@SuppressWarnings("unused")
+	private int howManyPass;
+
+	/*
+	 * This variable will hold the length of each password the user wishes to
+	 * generate from the object, keeping in mind that characters operate on a
+	 * zero index counter
+	 */
+	@SuppressWarnings("unused")
+	private int passLength;
+
+	/*
+	 * This is the default constructor for the {@link PassGenerator}. It will
+	 * know the length of each password, as well as how many, from passed in
+	 * parameters
+	 * 
+	 * @param howMany The number of passwords that will be associated with this
+	 * object
+	 * 
+	 * @param length The length of each password that will be associated with
+	 * this object, keeping in mind that characters operate on a zero index
+	 * counter
+	 */
+	public PassGenerator(int howMany, int length) {
+		howManyPass = howMany;
+		passLength = length;
+	}
+
+	/*
+	 * This method will generate the passwords for the user
+	 */
+	public void generate() {
+		
+	}
 }
