bca384ceacb3ae38ff7808da3cd9a7e310f042e1
Ismail A <ieabbas@users.noreply.github.com>
2016-05-31 23:54:19 -0700

    Create Challenge_006.java
    - There needs to be a way to format pi to "n" decimal points

---
+++ Easy-Challenges../Challenge-006../Challenge_006.java
@@ -0,0 +1,22 @@
+package challenge_006;
+
+/**
+ * This repository contains the challenges posted from the
+ * DailyProgrammerChallenges GitHub repository that can be found at
+ * https://github.com/FreddieV4/DailyProgrammerChallenges.
+ * 
+ * Challenge 006 : You're challenge for today is to create a program that can
+ * calculate pi accurately to at least 30 decimal places. Try not to cheat :)
+ * 
+ * Solution provided by : Ismail A.
+ * 
+ * @author ieabbas
+ */
+public class Challenge_006 {
+
+	public static void main(String[] args) {
+		System.out.println("Here is Pi, calculated to at least 30 decimal places:\n");
+		System.out.format("%f", Math.PI);
+	}
+
+}
