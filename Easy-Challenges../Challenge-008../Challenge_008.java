0fc2ee6ec1a8da14c6a928b438c19a6ee29230f7
Ismail A <ieabbas@users.noreply.github.com>
2016-06-09 00:57:04 -0700

    Finished Challenge_008
    - Added grammar fix for "1 bottle of beer" line

--- Easy-Challenges../Challenge-008../Challenge_008.java
+++ Easy-Challenges../Challenge-008../Challenge_008.java
@@ -20,12 +20,22 @@ public class Challenge_008 {
 		String lyric2 = " bottles of beer,";
 		String lyric3 = " take one down, pass it around,";
 		String lyric4 = " bottles of beer on the wall!";
-		
-		
-		
+
+		String lyric5 = " bottle of beer on the wall,";
+		String lyric6 = " bottle of beer,";
+		String lyric7 = " take one down, pass it around,";
+		String lyric8 = " bottle of beer on the wall!";
+
+		int count = 100;
+
 		for (int i = 0; i < 99; i++) {
-			int count = 99;
-			System.out.println(count - i);
+			if (count > 2) {
+				System.out.println((count - 1) + lyric1 + (count - 1) + lyric2 + lyric3 + (count - 1) + lyric4);
+			}
+			if (count == 2) {
+				System.out.println((count - 1) + lyric5 + (count - 1) + lyric6 + lyric7 + (count - 1) + lyric8);
+			}
+			count--;
 		}
 	}
 
