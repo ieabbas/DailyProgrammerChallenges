08987cd8973ab4e5f27708543e2c791c7a5d04bf
Ismail A <ieabbas@users.noreply.github.com>
2016-06-09 02:15:26 -0700

    UNFINISHED
    - General implementation needed

--- Easy-Challenges../Challenge-005../Challenge_005.java
+++ Easy-Challenges../Challenge-005../Challenge_005.java
@@ -7,7 +7,7 @@ package challenge_005;
  * 
  * Challenge 005 : Your challenge for today is to create a program which is
  * password protected, and wont open unless the correct user and password is
- * given.For extra credit, have the user and password in a seperate .txt file.
+ * given. For extra credit, have the user and password in a seperate .txt file.
  * For even more extra credit, break into your own program :)
  * 
  * Solution provided by : Ismail A.
