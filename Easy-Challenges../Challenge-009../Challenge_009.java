d431401abf403359a778901137d289a7c0102fc1
Ismail A <ieabbas@users.noreply.github.com>
2016-06-09 13:01:43 -0700

    Update Challenge_009.java
    - Unsure if Radix Sort is correctly working, needs further testing

--- Easy-Challenges../Challenge-009../Challenge_009.java
+++ Easy-Challenges../Challenge-009../Challenge_009.java
@@ -17,34 +17,76 @@ import java.io.*;
  */
 public class Challenge_009 {
 
-	// The logic here is that the Buffered reader will take in the input
-	// After that the String array will split the input by whitespace
-	// Finally the integer array will
+	// The logic here is that the Buffered reader will take in the input.
+	// After that the String array will split the input by whitespace.
+	// Finally the integer array will take the raw values to be sorted and
+	// printed later.
 	public static void main(String[] args) throws Exception {
-		System.out.println("Please enter digits continuously, separated by space, ending by hitting enter.");
+		System.out.println("Please enter digits continuously, separated by a single space, ending by hitting enter.");
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		String[] input = reader.readLine().split(" ");
 		int[] intArray = new int[input.length];
+		int[] outputArray = new int[intArray.length];
 
 		// Parsing and assigning int values
 		for (int i = 0; i < input.length; i++) {
 			intArray[i] = Integer.parseInt(input[i]);
 		}
 
-		// Printing the values
+		// Printing the values for confirmation
 		for (int i = 0; i < input.length; i++) {
 			System.out.println("You entered the number(s) " + intArray[i] + " ");
 		}
 
-		/*
-		 * // The process of taking input and assigning it to a char array int
-		 * input = kb.nextInt(); int[] inputArray = new int[input];
-		 * System.out.println(inputArray.length); for (int i = 0; i <
-		 * inputArray.length; i++) { inputArray[i] = input; }
-		 * 
-		 * int output; for (int i = 0; i < inputArray.length; i++) { output = ('
-		 * ' + (inputArray[i] - 64) + ' '); System.out.println(output); }
-		 */
+		// Sorting the values
+		outputArray = radixSort(intArray, 1);
+
+		// Printing the sorted values
+		System.out.print("\nYour sorted values are\n");
+		for (int i : outputArray) {
+			System.out.println(i);
+		}
+
+	}
+
+	/*
+	 * The following methods and auxiliary methods are pulled from previous
+	 * academic assignments on sorting algorithms. All rights are reserved to
+	 * Professor Edwin Rodriguez of Cal Poly Pomona.
+	 */
+	public static int[] radixSort(int[] numbers, int radix) {
+		int[] result = numbers;
+
+		for (int pos = 1; pos <= radix; ++pos) {
+			result = modCountingSort(result, pos);
+		}
+
+		return result;
+	}
+
+	public static int[] modCountingSort(int[] numbers, int pos) {
+		int[] counter = new int[10];
+		int[] result = new int[numbers.length];
+
+		for (int i = 0; i < numbers.length; ++i) {
+			++counter[getDigit(numbers[i], pos)];
+		}
+
+		for (int i = 1; i < counter.length; ++i) {
+			counter[i] += counter[i - 1];
+		}
+
+		for (int i = result.length - 1; i >= 0; --i) {
+			result[--counter[getDigit(numbers[i], pos)]] = numbers[i];
+		}
+
+		return result;
+	}
+
+	private static int getDigit(int number, int pos) {
+		int pow = (int) Math.pow(10, pos);
+		int rem = number % pow;
+		return rem / (pow / 10);
 	}
 
 }
