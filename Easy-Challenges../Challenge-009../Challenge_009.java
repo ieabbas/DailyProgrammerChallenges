ac0c7b1a8db515246975098317a89bb3f082a419
Ismail A <ieabbas@users.noreply.github.com>
2016-06-09 12:24:33 -0700

    Update Challenge_009.java
    - Tried a second algorithm with more success, original algorithm commented out

--- Easy-Challenges../Challenge-009../Challenge_009.java
+++ Easy-Challenges../Challenge-009../Challenge_009.java
@@ -1,6 +1,7 @@
 package challenge_009;
 
-import java.util.*;
+import java.io.*;
+
 /**
  * This repository contains the challenges posted from the
  * DailyProgrammerChallenges GitHub repository that can be found at
@@ -16,30 +17,34 @@ import java.util.*;
  */
 public class Challenge_009 {
 
-	// The logic here is that you will take the input as a String,
-	// turn that into in an array of characters, then be able to
-	// numerically order each position of the input
+	// The logic here is that the Buffered reader will take in the input
+	// After that the String array will split the input by whitespace
+	// Finally the integer array will
+	public static void main(String[] args) throws Exception {
+		System.out.println("Please enter digits continuously, separated by space, ending by hitting enter.");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = reader.readLine().split(" ");
+		int[] intArray = new int[input.length];
 
-	// Maybe this should be an int array so output can more easily be sorted?
-	@SuppressWarnings("resource")
-	public static void main(String[] args) {
-		Scanner kb = new Scanner(System.in);
-		System.out.println("Please enter digits continuously, ending by hitting enter.");
-
-		// The process of taking input and assigning it to a char array
-		String input = kb.nextLine();
-		char[] inputArray = new char[input.length()];
-		for (int i = 0; i < inputArray.length; i++) {
-			inputArray[i] = input.charAt(i);
+		// Parsing and assigning int values
+		for (int i = 0; i < input.length; i++) {
+			intArray[i] = Integer.parseInt(input[i]);
 		}
 
-		int output;
-		for (int i = 0; i < inputArray.length; i++) {
-			output = (char) (' ' + (inputArray[i]));
-			System.out.println(output);
+		// Printing the values
+		for (int i = 0; i < input.length; i++) {
+			System.out.println("You entered the number(s) " + intArray[i] + " ");
 		}
-		System.exit(0);
 
+		/*
+		 * // The process of taking input and assigning it to a char array int
+		 * input = kb.nextInt(); int[] inputArray = new int[input];
+		 * System.out.println(inputArray.length); for (int i = 0; i <
+		 * inputArray.length; i++) { inputArray[i] = input; }
+		 * 
+		 * int output; for (int i = 0; i < inputArray.length; i++) { output = ('
+		 * ' + (inputArray[i] - 64) + ' '); System.out.println(output); }
+		 */
 	}
 
 }
