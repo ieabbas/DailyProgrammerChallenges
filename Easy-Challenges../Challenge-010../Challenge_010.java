ce78dae5ab9eee2e4c063c83f97d53749461b180
Ismail A <ieabbas@users.noreply.github.com>
2016-06-10 13:46:16 -0700

    UNFINISHED
    - Let the implementation begin

---
+++ Easy-Challenges../Challenge-010../Challenge_010.java
@@ -0,0 +1,32 @@
+package challenge_010;
+
+/**
+ * This repository contains the challenges posted from the
+ * DailyProgrammerChallenges GitHub repository that can be found at
+ * https://github.com/FreddieV4/DailyProgrammerChallenges.
+ * 
+ * Challenge 010 : The exercise today asks you to validate a telephone number,
+ * as if written on an input form. Telephone numbers can be written as ten
+ * digits, or with dashes, spaces, or dots between the three segments, or with
+ * the area code parenthesized; both the area code and any white space between
+ * segments are optional. Thus, all of the following are valid telephone
+ * numbers: 1234567890, 123-456-7890, 123.456.7890, (123)456-7890, (123)
+ * 456-7890 (note the white space following the area code), and 456-7890.
+ * 
+ * The following are not valid telephone numbers: 123-45-6789, 123:4567890, and
+ * 123/456-7890.
+ * 
+ * source: programmingpraxis.com
+ * 
+ * Solution provided by : Ismail A.
+ * 
+ * @author ieabbas
+ */
+public class Challenge_010 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
