9c97c9fc223006c3bfa9f13eaf0576feaec226bf
Ismail A <ieabbas@users.noreply.github.com>
2016-06-10 13:57:01 -0700

    UNFINISHED
    - Let the implementation begin

---
+++ Easy-Challenges../Challenge-011../Challenge_011.java
@@ -0,0 +1,30 @@
+package challenge_011;
+
+import java.util.*;
+
+/**
+ * This repository contains the challenges posted from the
+ * DailyProgrammerChallenges GitHub repository that can be found at
+ * https://github.com/FreddieV4/DailyProgrammerChallenges.
+ * 
+ * Challenge 011 : The program should take three arguments. The first will be a
+ * day, the second will be month, and the third will be year. Then, your program
+ * should compute the day of the week that date will fall on.
+ * 
+ * Solution provided by : Ismail A.
+ * 
+ * @author ieabbas
+ */
+public class Challenge_011 {
+
+  //Assume that the input is either a String or an int, not both
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		String day;
+		String month;
+		int year;
+		
+		System.out.println();
+	}
+
+}
