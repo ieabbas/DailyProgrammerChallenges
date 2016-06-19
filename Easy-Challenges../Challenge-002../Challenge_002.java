b2974b8b7bb210661f3a59998624c5cfd82e39d9
Ismail A <ieabbas@users.noreply.github.com>
2016-06-19 15:02:22 -0700

    Finished Challenge_002
    - Redone without the use of an extra class for a simple calculator

--- Easy-Challenges../Challenge-002../Challenge_002.java
+++ Easy-Challenges../Challenge-002../Challenge_002.java
@@ -1,6 +1,8 @@
 package challenge_002;
 
 import java.util.*;
+import java.io.*;
+
 /**
  * This repository contains the challenges posted from the
  * DailyProgrammerChallenges GitHub repository that can be found at
@@ -17,42 +19,79 @@ import java.util.*;
  */
 public class Challenge_002 {
 
-	@SuppressWarnings("rawtypes")
-	public static void main(String[] args) {
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws Exception {
 		// Variables needed for calculation
-		Calculator calc = new Calculator();
 		Scanner kb = new Scanner(System.in);
-		int n1;
-		int n2;
 		int choice;
 
 		do {
-			System.out.println("Enter the two numbers you wish to perform operations upon:");
-			n1 = kb.nextInt();
-			n2 = kb.nextInt();
+			System.out.println(
+					"Enter the the numbers you wish to perform operations upon, separated by a space and when finished press enter:");
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			String[] input = reader.readLine().split(" ");
+			int[] intArray = new int[input.length];
+
+			// Parsing and assigning int values
+			for (int i = 0; i < input.length; i++) {
+				intArray[i] = Integer.parseInt(input[i]);
+			}
 
 			System.out.println(
-					"Select the operation you wish to perform. Type and enter a capital \"D\" to stop: \n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division");
+					"Select the operation you wish to perform. Type and enter \"5\" to stop: \n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division");
 
 			choice = kb.nextInt();
 			switch (choice) {
 			case 1:
-				System.out.println("\nThe answer is " + calc.addNum(n1, n2) + "\n");
+				System.out.println("\nThe answer is " + addNum(intArray) + "\n");
 				break;
 			case 2:
-				System.out.println("\nThe answer is " + calc.subtractNum(n1, n2) + "\n");
+				System.out.println("\nThe answer is " + subtractNum(intArray) + "\n");
 				break;
 			case 3:
-				System.out.println("\nThe answer is " + calc.multiplyNum(n1, n2) + "\n");
+				System.out.println("\nThe answer is " + multiplyNum(intArray) + "\n");
 				break;
 			case 4:
-				System.out.println("\nThe answer is " + calc.divideNum(n1, n2) + "\n");
+				System.out.println("\nThe answer is " + divideNum(intArray) + "\n");
+				break;
+			case 5:
 				break;
 			default:
 				System.out.println("\n" + choice + " isn't an operation" + "\n");
 			}
-		} while (n1 != 0 && n2 != 0);
-		kb.close();
+		} while (true);
+	}
+
+	public static int addNum(int[] array) {
+		int output = 0;
+		for (int i = 0; i < array.length; i++) {
+			output += array[i];
+		}
+		return output;
+	}
+
+	public static int subtractNum(int[] array) {
+		int output = array[0];
+		for (int i = 1; i < array.length; i++) {
+			output -= array[i];
+		}
+		return output;
+	}
+
+	public static int multiplyNum(int[] array) {
+		int output = 1;
+		for (int i = 0; i < array.length; i++) {
+			output *= array[i];
+		}
+		return output;
+	}
+
+	public static int divideNum(int[] array) {
+		int output = array[0];
+		for (int i = 1; i < array.length; i++) {
+			output /= array[i];
+		}
+		return output;
 	}
 
 }
