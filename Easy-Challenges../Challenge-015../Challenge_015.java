4a1b4c00709a21138dae2947e4a3449dae8ae4f9
Ismail A <ieabbas@users.noreply.github.com>
2016-06-19 16:51:12 -0700

    UNFINISHED

---
+++ Easy-Challenges../Challenge-015../Challenge_015.java
@@ -0,0 +1,21 @@
+package challenge_015;
+
+/**
+ * This repository contains the challenges posted from the
+ * DailyProgrammerChallenges GitHub repository that can be found at
+ * https://github.com/FreddieV4/DailyProgrammerChallenges.
+ * 
+ * Challenge 015 : Write a program to left or right justify a text file
+ * 
+ * Solution provided by: Ismail A.
+ * 
+ * @author ieabbas
+ */
+public class Challenge_015 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
