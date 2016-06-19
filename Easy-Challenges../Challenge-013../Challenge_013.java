81e4085e76545dbaa4c6cc208d3190130af49948
Ismail A <ieabbas@users.noreply.github.com>
2016-06-19 16:49:38 -0700

    UNFINISHED

---
+++ Easy-Challenges../Challenge-013../Challenge_013.java
@@ -0,0 +1,23 @@
+package challenge_013;
+
+/**
+ * This repository contains the challenges posted from the
+ * DailyProgrammerChallenges GitHub repository that can be found at
+ * https://github.com/FreddieV4/DailyProgrammerChallenges.
+ * 
+ * Challenge 013 : Find the number of the year for the given date. For example,
+ * January 1st would be 1, and December 31st is 365. For extra credit, allow it
+ * to calculate leap years, as well.
+ * 
+ * Solution provided by: Ismail A.
+ * 
+ * @author ieabbas
+ */
+public class Challenge_013 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
