7fc41e66e22f759aee694d9b8a11a6b1fc884ff5
Ismail A <ieabbas@users.noreply.github.com>
2016-06-19 16:50:21 -0700

    UNFINISHED

---
+++ Easy-Challenges../Challenge-014../Challenge_014.java
@@ -0,0 +1,28 @@
+package challenge_014;
+
+/**
+ * This repository contains the challenges posted from the
+ * DailyProgrammerChallenges GitHub repository that can be found at
+ * https://github.com/FreddieV4/DailyProgrammerChallenges.
+ * 
+ * Challenge 014 : Input: list of elements and a block size k or some other
+ * variable of your choice
+ * 
+ * Output: return the list of elements with every block of k elements reversed,
+ * starting from the beginning of the list.
+ * 
+ * For instance, given the list 12, 24, 32, 44, 55, 66 and the block size 2, the
+ * result is 24, 12, 44, 32, 66, 55.
+ * 
+ * Solution provided by: Ismail A.
+ * 
+ * @author ieabbas
+ */
+public class Challenge_014 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
