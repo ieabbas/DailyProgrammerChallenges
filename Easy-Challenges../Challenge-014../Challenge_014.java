92c2670c664664936ce89bbaf942b6c17104262b
Ismail A <ieabbas@users.noreply.github.com>
2016-06-24 02:48:31 -0700

    UNFINISHED
    - Only requires the size "k" element reversal (swap)

--- Easy-Challenges../Challenge-014../Challenge_014.java
+++ Easy-Challenges../Challenge-014../Challenge_014.java
@@ -1,6 +1,7 @@
 package challenge_014;
 
 import java.io.*;
+import java.util.Scanner;
 
 /**
  * This repository contains the challenges posted from the
@@ -12,7 +13,7 @@ import java.io.*;
  * 
  * Output: return the list of elements with every block of k elements reversed,
  * starting from the beginning of the list.
- * 
+ *
  * For instance, given the list 12, 24, 32, 44, 55, 66 and the block size 2, the
  * result is 24, 12, 44, 32, 66, 55.
  * 
@@ -23,11 +24,24 @@ import java.io.*;
 public class Challenge_014 {
 
 	@SuppressWarnings("unused")
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		int k;
 		String[] input;
+		Scanner kb = new Scanner(System.in);
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("Enter a list of elements, each element separated by a space:");
 
+		// Parse the input for strings, place them into an array
+		System.out.print("Enter a list of elements, each element separated by a space:");
+		input = reader.readLine().split(" ");
+
+		for (String i : input) {
+			System.out.print(i + " ");
+		}
+
+		System.out.println("Enter size k elements to be reversed: ");
+		k = kb.nextInt();
+
+		kb.close();
 	}
 
 	/*
