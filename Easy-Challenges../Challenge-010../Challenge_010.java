a6343084df23461ebf1695c04049abcd512c7682
Ismail A <ieabbas@users.noreply.github.com>
2016-06-26 14:43:28 -0700

    UNFINISHED
    Cases to check for:
    - where the symbols and numbers are placed 
    - if invalid symbols are used, how to check for it

--- Easy-Challenges../Challenge-010../Challenge_010.java
+++ Easy-Challenges../Challenge-010../Challenge_010.java
@@ -28,9 +28,8 @@ import java.util.*;
  */
 public class Challenge_010 {
 
-	@SuppressWarnings({ "unused", "resource" })
+	@SuppressWarnings({ "resource" })
 	public static void main(String[] args) {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		Scanner kb = new Scanner(System.in);
 		String input;
 
@@ -41,11 +40,12 @@ public class Challenge_010 {
 			// Check for the invalid conditions first, then everything else
 			// should
 			// be valid right?
-			if (validLength(input) == false || validSymbolNum(input) == false) {
+			if (validLength(input) == false || validSymbolNum(input) == false || validIntNum(input) == false) {
 				System.out.println("This is not a valid number");
 			} else {
 				System.out.println("This is a valid number.");
 			}
+			System.out.println("The symbol used in this number is " + whichSymbol(input) + "\n");
 		} while (true);
 
 	}
@@ -53,11 +53,11 @@ public class Challenge_010 {
 	/*
 	 * This method will check if there is a minimum number of 8 characters
 	 * (shortest combination of just numbers), or maximum number of 14
-	 * characters (numbers+symbols) in the entered phone number. It will return
-	 * true if it is valid, and false if otherwise.
+	 * characters (numbers+symbols+whitespace) in the entered phone number. It
+	 * will return true if it is valid, and false if otherwise.
 	 */
 	public static boolean validLength(String input) {
-		if (input.length() > 14 || input.length() < 8) {
+		if (input.length() > 15 || input.length() < 8) {
 			return false;
 		} else {
 			return true;
@@ -65,19 +65,67 @@ public class Challenge_010 {
 	}
 
 	/*
+	 * This method will check if the entered phone number has an incorrect
+	 * amount of symbols. This method also handles the issue of if there are an
+	 * appropriate number of integers in the given phone number. If there are
+	 * too little, then the number will have more symbols to satisfy the
+	 * character length requirement of the validLength() method. If there are
+	 * too many, the final else if statement will catch it. It will return true
+	 * if the expression satisfies these parameters, and false if otherwise.
+	 */
+	public static boolean validSymbolNum(String input) {
+		int[] symbolNum = { 0, 0 };
+		char[] symbols = { '-', '.' };
+
+		// Check each character of the input against each position of the
+		// symbols array
+		for (int i = 0; i < input.length(); i++) {
+			for (int j = 0; j < symbols.length; j++) {
+				if (input.charAt(i) == symbols[j]) {
+					symbolNum[j]++;
+				}
+			}
+		}
+
+		// A phone number can have a max of 2 hyphens, or 3 periods
+		if (symbolNum[0] > 2 || symbolNum[1] > 3) {
+			return false;
+		}
+		// If there are more than one of each, the number is invalid, you can't
+		// have both periods and hyphens
+		else if (symbolNum[0] > 0 && symbolNum[1] > 0) {
+			return false;
+		}
+		// If there are no symbols at all, there must be a maximum of 10
+		// characters (i.e. 1234567890)
+		else if (symbolNum[0] == 0 && symbolNum[1] == 0 && input.length() != 10) {
+			return false;
+		}
+		// If there are invalid symbols, anything other than a hyphen or period,
+		// the number is invalid
+		else if () {
+
+		} else {
+			return true;
+		}
+
+	}
+
+	/*
 	 * This method will check if there is an appropriate number of integer
 	 * values within the input
 	 */
+	@SuppressWarnings("unused")
 	public static boolean validIntNum(String input) {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		return true;
 	}
 
 	/*
-	 * This method will check if the entered phone number has an incorrect
-	 * amount of symbols. It will return true if the expression satisfies these
-	 * parameters, and false if otherwise.
+	 * This method will return what symbol is being used in the phone number,
+	 * exlusive of parenthesis. An auxiliary method to validIntNum().
 	 */
-	public static boolean validSymbolNum(String input) {
+	public static char whichSymbol(String input) {
 		int[] symbolNum = { 0, 0, 0 };
 		char[] symbols = { '-', '.' };
 
@@ -91,15 +139,12 @@ public class Challenge_010 {
 			}
 		}
 
-		// A phone number can have a max of 2 hyphens, and 3 periods
-		if (symbolNum[0] > 2) {
-			return false;
-		} else if (symbolNum[1] > 3) {
-			return false;
+		if (symbolNum[0] >= 1) {
+			return '-';
+		} else if (symbolNum[1] >= 1) {
+			return '.';
 		} else {
-			return true;
+			return 'E';
 		}
-
 	}
-
 }
