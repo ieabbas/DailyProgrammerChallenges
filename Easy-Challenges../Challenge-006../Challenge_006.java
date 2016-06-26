c3589ffc088d7fe60ae2b012d1b6c30b92ea9785
Ismail A <ieabbas@users.noreply.github.com>
2016-06-26 14:59:50 -0700

    UNFINISHED
    - Attempted to calculate pi as a limit

--- Easy-Challenges../Challenge-006../Challenge_006.java
+++ Easy-Challenges../Challenge-006../Challenge_006.java
@@ -16,14 +16,15 @@ public class Challenge_006 {
 
 	public static void main(String[] args) {
 		System.out.println("Here is Pi, calculated to at least 30 decimal places:\n");
-		char pi = '3';
+		int pi = 3;
 
 		for (int i = 3; i < 30; i++) {
 			int n1 = i + 1;
-			pi = (char) (pi + ((char)n1));
+			pi = (pi + ((char) n1));
 		}
 		System.out.println(pi);
 		System.out.println(Math.PI);
+		System.out.println(120 * (Math.sin(Math.toRadians(180 / 120))));
 
 	}
 }
