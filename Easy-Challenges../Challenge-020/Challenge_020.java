f5721deff7bffb6f98a140b5f67d0a435ccb9939
Ismail A <ieabbas@users.noreply.github.com>
2017-06-17 02:40:09 -0700

    Create Challenge_020.java

---
+++ Easy-Challenges../Challenge-020/Challenge_020.java
@@ -0,0 +1,26 @@
+package challenge_020;
+
+/**
+ * This repository contains the challenges posted from the
+ * DailyProgrammerChallenges GitHub repository that can be found at
+ * https://github.com/FreddieV4/DailyProgrammerChallenges.
+ * 
+ * Challenge 020 : Create a program that will find all prime numbers below 2000
+ * 
+ * Solution provided by: Ismail A.
+ * 
+ * @author ieabbas
+ */
+
+public class Challenge_020 {
+
+	public static void main(String[] args) throws Exception {
+
+		// Reference the wiki on "formula for primes" to get the basic
+		// calculations
+		System.out.println("This program will list out all prime numbers under 2000.");
+		Thread.sleep(2500);
+
+	}
+
+}
